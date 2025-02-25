54f80715c4e1b8fd3dab3dd4bfe9fdd713b881b5
nhuutrong203 <164836167+nhuutrong203@users.noreply.github.com>
2025-02-25 12:09:35 +0700

    Delete FastAPI directory

--- FastAPI/services/creature/error.py
+++
@@ -1,9 +0,0 @@
-# Data exceptions
-
-class Missing(Exception):
-    def __init__(self, msg:str):
-        self.msg = msg
-
-class Duplicate(Exception):
-    def __init__(self, msg:str):
-        self.msg = msg
